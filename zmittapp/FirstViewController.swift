acf12ab1b1773b691362af8721714241d774b6e5
Brian Mc Alister <brian@mcalister.ch>
2014-11-03 09:19:53 +0100

    Initial Commit

---
+++ zmittapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  zmittapp
+//
+//  Created by Brian Mc Alister on 03/11/14.
+//  Copyright (c) 2014 Brian Mc Alister. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ zmittapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  zmittapp
+//
+//  Created by Brian Mc Alister on 03/11/14.
+//  Copyright (c) 2014 Brian Mc Alister. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
