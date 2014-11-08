c4e8ab0d17f52f9f913fa8558fbee6095a3e2420
Brian Mc Alister <brian@mcalister.ch>
2014-11-08 15:37:27 +0100

    all views

---
+++ zmittapp/dailymenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  dailymenuViewController.swift
+//  zmittapp
+//
+//  Created by Brian Mc Alister on 08/11/14.
+//  Copyright (c) 2014 Brian Mc Alister. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class dailymenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // setup background
+         self.view.backgroundColor = UIColor.clearColor()
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
