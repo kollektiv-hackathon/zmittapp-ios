47d46a32cffee9ba3846b417cac8958c093018c4
Marc Juchli <mail@marcjuch.li>
2014-11-08 18:45:49 +0100

    prototype for nearby more or less done

--- zmittapp/allRestaurantsViewController.swift
+++ zmittapp/allRestaurantsViewController.swift
@@ -79,7 +79,8 @@ class allRestaurantsViewController: UITableViewController, UITableViewDelegate, 
             lat:    restaurant["lat"] as Double,
             lng:    restaurant["lon"] as Double,
             email:  restaurant["email"] as String,
-            menu:   [menuData]()
+            menu:   [menuData](),
+            distance: 0.0
         )
         
         // instantiate new Restaurant with fetched data
